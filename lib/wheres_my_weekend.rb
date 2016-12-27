a99c962746c071f0f302c4b0f10f7741031682c2
Odinei Rocha <odinei.ribeiro@live.com>
2016-12-26 22:47:02 -0200

    method next_weekend

--- lib/wheres_my_weekend.rb
+++ lib/wheres_my_weekend.rb
@@ -1,8 +1,19 @@
 require "wheres_my_weekend/version"
+require 'pry'
 
 class Time
   def weekend_day?
     saturday? || sunday?
+  end
+
+  def next_weekend
+    weekend_day? ? [self, plus_one_day] : plus_one_day.next_weekend
+  end
+
+  private
+
+  def plus_one_day
+    self + (60 * 60 * 24)
   end
 end
 
